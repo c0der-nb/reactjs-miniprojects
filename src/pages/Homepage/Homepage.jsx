a9d3973ce7a5d63981dbf70bba8c5a7c248c5717
Aditya Gupta <gaditya060@gmail.com>
2024-05-29 00:30:31 +0530

    Minor formatting

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -6,7 +6,7 @@ function Homepage() {
     return (
         <div className={styles.wrapper}>
             <h1>ReactJS Mini Projects</h1>
-            <Link to="/counter-app"><h3 className={styles.homepageH3}>Counter App</h3></Link>
+            <Link style={{marginTop: '30px'}} to="/counter-app"><h3 className={styles.homepageH3}>Counter App</h3></Link>
             <Link to="/react-calculator"><h3 className={styles.homepageH3}>React Calculator</h3></Link>
             <Link to="/login"><h3 className={styles.homepageH3}>Login</h3></Link>
             <Link to="/select-location"><h3 className={styles.homepageH3}>Select Location</h3></Link>
